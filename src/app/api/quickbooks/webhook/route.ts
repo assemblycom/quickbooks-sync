9bdea7853dde5d1bad330438eae0b5892f204676
SandipBajracharya <sndp106.sb@gmail.com>
2026-04-09 14:20:26 +0545

    hotfix: increase max duration of serverless to 5 mins

--- src/app/api/quickbooks/webhook/route.ts
+++ src/app/api/quickbooks/webhook/route.ts
@@ -1,4 +1,6 @@
 import { withErrorHandler } from '@/app/api/core/utils/withErrorHandler'
 import { captureWebhookEvent } from '@/app/api/quickbooks/webhook/webhook.controller'
 
+export const maxDuration = 300 // 5 minutes
+
 export const POST = withErrorHandler(captureWebhookEvent)
